b898a7f8a5a825108a3d7277c4c180232e650f00
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2024-03-20 18:36:34 +0000

    add logo

--- frontend/src/Api/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const axiosClient = axios.create({
-    // baseURL : import.meta.env.VITE_BACKEND_URL ,
-    baseURL : "http://localhost:8000" ,
-    withCredentials: true,
-    withXSRFToken: true,
-
-})
